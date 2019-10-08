cbe92036e87f0f3eb7cd9270ed022683f3518067
Jes'kad Ad'aryc <47439649+LegoFan48737@users.noreply.github.com>
2019-10-08 17:23:12 -0400

    Update EventListener.php

--- src/Kad/Core/EventListener.php
+++ src/Kad/Core/EventListener.php
@@ -2,10 +2,30 @@
 
 use pocketmine\event\Listener;
 use pocketmine\event\player\PlayerJoinEvent;
+use pocketmine\event\player\PlayerQuitEvent;
 
 class EventListener implements Listener {
+
     private $plugin;
 
     public function __construct(Main $plugin) {
         $this->plugin = $plugin
     }
+    public function onJoin(PlayerJoinEvent $event) {
+    $player = $event->getPlayer();
+    $name = $player->getName();
+    $event->setJoinMessage("§8•§r " . $name . " §8has joined");
+    $world = $this->getServer()->getLevelByName("world");
+    $x = 0;
+    $y = 66;
+    $z = 0;
+    $pos = new Position($x, $y, $z, $world);
+    $player->teleport($pos);
+    $player->setGamemode(1);
+    }
+    public function onQuit(PlayerQuitEvent, $event){
+        $player = $event->getPlayer();
+        $name = $event->getName();
+        $event->setQuitMessage("§8•§r" . $name . " §8has left");
+    }
+
