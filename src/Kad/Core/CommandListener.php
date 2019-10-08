b20e5b5f3e514f98f9022272cd16a7b78cefcdf8
Jes'kad Ad'aryc <47439649+LegoFan48737@users.noreply.github.com>
2019-10-08 17:30:38 -0400

    Update CommandListener.php

--- src/Kad/Core/CommandListener.php
+++ src/Kad/Core/CommandListener.php
@@ -7,6 +7,7 @@ use pocketmine\utils\TextFormat as TF;
 use pocketmine\level\Position;
 
 class CommandListener implements Listener {
+
     private $plugin;
 
     public function __construct(Main $plugin) {
