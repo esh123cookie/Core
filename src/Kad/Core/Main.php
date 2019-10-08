a27a16b14ce1bd8c171d875b446343a0d6ed93fd
Jes'kad Ad'aryc <47439649+LegoFan48737@users.noreply.github.com>
2019-10-08 17:26:44 -0400

    Cleared

--- src/Kad/Core/Main.php
+++ src/Kad/Core/Main.php
@@ -2,25 +2,15 @@
 
 namespace Kad\Core;
 
-use pocketmine\Player;
-use pocketmine\Player;
 use pocketmine\Server;
-use pocketmine\level\Position;
 use pocketmine\plugin\PluginBase;	
-use pocketmine\command\Command;
-use pocketmine\command\CommandSender;
-use pocketmine\utils\TextFormat as TF;
 use pocketmine\event\Listener;
-use pocketmine\event\player\PlayerJoinEvent;
-use pocketmine\event\player\PlayerQuitEvent;
-use pocketmine\event\player\PlayerRespawnEvent;
-use pocketmine\event\player\PlayerDeathEvent;
 
 class Main extends PluginBase implements Listener{
-	
-public $fts = "§a[§eCore§r]§a"
-	
+
 public function onEnable(){
     $this->getServer()->getPluginManager()->registerEvents($this, $this);
     $this->getServer()->getPluginManager()->registerEvents(new EventListener($this), $this);
+    $this->getServer()->getPluginManager()->registerEvents(new CommandListener($this), $this);
+    }
 }
