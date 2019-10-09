456ee94dae77867cb5a3d01f0a1a7a6e4e531beb
Jes'kad Ad'aryc <47439649+LegoFan48737@users.noreply.github.com>
2019-10-09 12:46:11 -0400

    Update MapReset.php

--- src/Kad/Core/MapReset.php
+++ src/Kad/Core/MapReset.php
@@ -1,1 +1,11 @@
 <?php
+
+class MapReset implements Listener {
+
+    private $plugin;
+
+    public function __construct(Main $plugin) {
+        $this->plugin = $plugin
+    }
+
+Wait a second, why would a map reset need a listener.....
