1d93d3e675c601c6db2af5f4fe132d632a7b69fd
Jes'kad Ad'aryc <47439649+LegoFan48737@users.noreply.github.com>
2019-10-08 21:09:57 -0400

    Started commands using Break method
    Lets hope I did the `break;` right.

--- src/Kad/Core/CommandListener.php
+++ src/Kad/Core/CommandListener.php
@@ -13,3 +13,26 @@ class CommandListener implements Listener {
     public function __construct(Main $plugin) {
         $this->plugin = $plugin
     }
+    public function onCommand(CommandSender $sender, Command $command, $label, array $args) : bool{
+        switch (strtolower($command->getName())) {
+            case "gmc":
+            if($sender instanceof Player) {
+                 if($sender->hasPermission("core.gmc.use")) {
+                     $sender->setGamemode(1);
+                     $sender->sendMessage($this->fts . TF::GREEN . "Your gamemode has been set to creative!");
+                 } else {
+                     $sender->sendMessage($this->fts . TF::RED . "An error has occurred. Please contact Jes'kad Ad'aryc#3845 on Discord to report this");
+                 }
+            }
+            break;
+
+            case "gms":
+            if($sender instanceof Player) {
+                 if($sender->hasPermission("core.gms.use")) {
+                     $sender->setGamemode(0);
+                     $sender->sendMessage($this->fts . TF::GREEN . "Your gamemode has been set to survival!");
+                 } else {
+                     $sender->sendMessage($this->fts . TF::RED . "An error has occurred. Please contact Jes'kad Ad'aryc#3845 on Discord to report this");
+                 }
+            }
+            break;
