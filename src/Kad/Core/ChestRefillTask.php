059e6081772ea1791299fedf5b1ac019a08026fe
Jes'kad Ad'aryc <47439649+LegoFan48737@users.noreply.github.com>
2019-10-18 17:14:00 -0400

    Began work

--- src/Kad/Core/ChestRefillTask.php
+++ src/Kad/Core/ChestRefillTask.php
@@ -1,2 +1,13 @@
 <?php
 
+use pocketmine\event\Listener;
+use pocketmine\utils\TextFormat as TF;
+
+class ChestRefillTask implements Listener {
+
+    private $plugin;
+
+    public function __construct(Main $plugin) {
+        $this->plugin = $plugin
+    }
+
